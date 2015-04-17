be41155c32919e5cb08654bb8c39a187b1a88f27
student <none@none.com>
2015-04-17 12:49:02 -0700

    Added CareerBuider files

---
+++ MyJobs/CareerBuilderJob.h
@@ -0,0 +1,13 @@
+//
+//  CareerBuilderJob.h
+//  MyJobs
+//
+//  Created by student on 4/17/15.
+//  Copyright (c) 2015 Joji Kubota. All rights reserved.
+//
+
+#ifndef MyJobs_CareerBuilderJob_h
+#define MyJobs_CareerBuilderJob_h
+
+
+#endif
---
+++ MyJobs/CareerBuilderJob.m
@@ -0,0 +1,9 @@
+//
+//  CareerBuilderJob.m
+//  MyJobs
+//
+//  Created by student on 4/17/15.
+//  Copyright (c) 2015 Joji Kubota. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
