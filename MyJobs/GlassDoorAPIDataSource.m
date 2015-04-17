97728a47a70ab41d41b914282e32c83e4b4601f0
student <none@none.com>
2015-04-17 14:19:16 -0700

    added monster & glass door data source

---
+++ MyJobs/GlassDoorAPIDataSource.m
@@ -0,0 +1,13 @@
+//
+//  GlassDoorAPIDataSource.m
+//  MyJobs
+//
+//  Created by student on 4/17/15.
+//  Copyright (c) 2015 Joji Kubota. All rights reserved.
+//
+
+#import "GlassDoorAPIDataSource.h"
+
+@implementation GlassDoorAPIDataSource
+
+@end
---
+++ MyJobs/MonsterDataSource.h
@@ -0,0 +1,18 @@
+//
+//  MonsterDataSource.h
+//  MyJobs
+//
+//  Created by student on 4/17/15.
+//  Copyright (c) 2015 Joji Kubota. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MonsterDataSource : NSObject
+
+- (instancetype) initWithURLString: (NSString *) urlString;
+- (NSMutableArray *) getAllJobs;
+- (NSInteger *) getNumberOfJobs;
+- (Job *) jobAtIndex: (NSInteger *) idx;
+
+@end
