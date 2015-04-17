9657919903827b46189746d3569b6b3addfb11ba
student <none@none.com>
2015-04-17 14:52:45 -0700

    commit

---
+++ MyJobs/MonsterDataSource.h
@@ -0,0 +1,13 @@
+//
+//  MonsterDataSource.h
+//  MyJobs
+//
+//  Created by student on 4/17/15.
+//  Copyright (c) 2015 Joji Kubota. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MonsterDataSource : NSObject
+
+@end
---
+++ MyJobs/MonsterDataSource.m
@@ -0,0 +1,13 @@
+//
+//  MonsterDataSource.m
+//  MyJobs
+//
+//  Created by student on 4/17/15.
+//  Copyright (c) 2015 Joji Kubota. All rights reserved.
+//
+
+#import "MonsterDataSource.h"
+
+@implementation MonsterDataSource
+
+@end
